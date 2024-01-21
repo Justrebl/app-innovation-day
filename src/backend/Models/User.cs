5354d39c3a553705aec49482fefad7e4631c05ab
Julien S <julien.strebler@outlook.fr>
2024-01-21 21:37:52 +0000

    feat : Add an azure function backend

---
+++ src/backend/Models/User.cs
@@ -0,0 +1,15 @@
+using Azure;
+using Azure.Data.Tables;
+
+namespace CafeReadConf
+{
+    public class UserEntity : ITableEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
