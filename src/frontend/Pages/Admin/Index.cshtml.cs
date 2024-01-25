214e531266ad6bfa0f51e94bbed47fc45f4f9bb4
Julien Strebler <durieljulien@gmail.com>
2024-01-25 14:30:21 +0100

    feat : Working Add User API

--- src/frontend/Pages/Admin/Index.cshtml.cs
+++ src/frontend/Pages/Admin/Index.cshtml.cs
@@ -1,5 +1,8 @@
+using CafeReadConf.Frontend.Models;
+using CafeReadConf.Frontend.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Azure;
 using Microsoft.Net.Http.Headers;
 
 namespace CafeReadConf.Pages.Admin
@@ -7,43 +10,47 @@ namespace CafeReadConf.Pages.Admin
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private IHttpClientFactory _clientFactory;
+        private IUserService _userService;
         private IConfiguration _configuration;
+        public string? Secret { get; set; }
+        [BindProperty]
+        public Usermodel Input { get; set; }
 
-        public void OnGet()
+        public PageResult OnGet()
         {
+            return Page();
         }
 
-        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory, IConfiguration configuration)
+        public IndexModel(ILogger<IndexModel> logger, IUserService userService, IConfiguration configuration)
         {
             _logger = logger;
-            _clientFactory = clientFactory;
+            _userService = userService;
             _configuration = configuration;
+            Secret = _configuration.GetValue<string>("secret");
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError(string.Empty, "Please fill in all the inputs before hitting the \"Add New User\" Button");
+            }
+            else
+            {
+                try
+                {
+                    _userService.AddUser(Input);
+                    TempData["UserAddedOK"] = "User added successfully";
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+                    ModelState.AddModelError(string.Empty, "Something went wrong while adding the user. please try again later");
+                }
+            }
 
-            
-            var request = new HttpRequestMessage(
-            HttpMethod.Get,
-            new Uri(baseUri: new Uri(_configuration["BACKEND_API_URL"]), relativeUri: "api/secret")){
-                Headers ={{ HeaderNames.Accept, "application/json"}}
-            };
 
-            var client = _clientFactory.CreateClient();
-            var response = await client.SendAsync(request);
-            // if (response.IsSuccessStatusCode)
-            // {
-            //     ViewData["Secret"] = await response.Content.ReadAsStringAsync();
-            // }
-            // else
-            // {
-            //     ViewData["Secret"] = "Error";
-            // }
             return Page();
         }
-
-        
     }
 }
